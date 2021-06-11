ec322728bee3dcb908184c3f8611fb7063a08969
unknown <tomasmiranda98@hotmail.com>
2021-06-11 13:23:05 -0300

    se agrega el caso que se trate un pedido erroneo

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -1,6 +1,7 @@
 package ar.edu.unahur.obj2.servidorWeb
 
 import java.time.LocalDateTime
+import kotlin.concurrent.timer
 
 // Para no tener los códigos "tirados por ahí", usamos un enum que le da el nombre que corresponde a cada código
 // La idea de las clases enumeradas es usar directamente sus objetos: CodigoHTTP.OK, CodigoHTTP.NOT_IMPLEMENTED, etc
@@ -19,16 +20,31 @@ class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime){
 }
 class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido){}
 
-class ServidorWeb(){
+class ServidorWeb() {
   val cantidadDePedidos = mutableListOf<Pedido>()
   var pedidosExistosos = 0
-  var pedidoActual: Pedido? = null
+  //en teoria el lateinit evita q se envie un null
+  lateinit var pedidoActual: Pedido
 
-  fun recibirPedido(pedido: Pedido){
+  //creo que recibir y enviar deberian estar en una misma funcion
+
+  fun recibirPedido(pedido: Pedido) {
     pedidoActual = pedido
   }
 
-  fun enviarRespuesta(){}
+  fun enviarRespuesta(): Respuesta? {
+    var respuesta: Respuesta? = null
+    if (!pedidoActual.protocoloUrl().equals("http")) {
+      //solucion parcial para los 10 milisegundos
+      val tiempo1 = LocalDateTime.now()
+      val tiempo2 = tiempo1.plusNanos(10)
+      val nano = tiempo2.nano - tiempo1.nano
+      //no se si se devuelve asi un enun
+      respuesta = Respuesta(CodigoHttp.NOT_IMPLEMENTED, "todavia no funciona", nano, pedidoActual)
+
+    }
+    return respuesta
+  }
 
   fun fueUnPedidoExitoso(){pedidosExistosos+=1}
 }
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -2,6 +2,7 @@ package ar.edu.unahur.obj2.servidorWeb
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
@@ -21,5 +22,41 @@ class ServidorWebTest : DescribeSpec({
     }
   }
   describe("Un servidor web") {
+    val server = ServidorWeb()
+
+    describe("un pedido erroneo"){
+      val pedidoConError = Pedido("194.168.2.0","https://pepito.com.ar/documentos/doc1.html",LocalDateTime.now())
+
+      it("devuele el codigo 501"){
+        server.recibirPedido(pedidoConError)
+        //suponiendo que tiene q devolver 501, en el caso que no eliminar el ultimo .codigo
+        val cod = server.enviarRespuesta()?.codigo?.codigo
+        cod.shouldBe(501)
+      }
+
+      it("no devuelve el codigo de errror de esta ok(200) "){
+        server.recibirPedido(pedidoConError)
+        val cod = server.enviarRespuesta()?.codigo?.codigo
+        cod.shouldNotBe(200)
+      }
+
+      it("el tiempo que tarda es de 10 milisegundos"){
+        //10 mili
+        val tiempo1 = LocalDateTime.now()
+        val tiempo2 = tiempo1.plusNanos(10)
+        val nano = tiempo2.nano - tiempo1.nano
+
+        server.recibirPedido(pedidoConError)
+        val respuesta = server.enviarRespuesta()
+        respuesta?.tiempo.shouldBe(nano)
+      }
+
+      it("el tiempo no sera distinto de 10 milisegundos"){
+        server.recibirPedido(pedidoConError)
+        val respuesta = server.enviarRespuesta()
+        respuesta?.tiempo.shouldNotBe(15)
+      }
+    }
+
   }
 })
