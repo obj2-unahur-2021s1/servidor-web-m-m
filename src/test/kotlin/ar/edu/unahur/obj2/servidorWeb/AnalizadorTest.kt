614c0b75389fc1fb56c1eca05b8504d3b7fefa1c
unknown <tomasmiranda98@hotmail.com>
2021-06-19 16:36:43 -0300

    se agrega test de analizador al agregar

--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/AnalizadorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/AnalizadorTest.kt
@@ -1,6 +1,8 @@
 package ar.edu.unahur.obj2.servidorWeb
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
 import java.time.LocalDateTime
 
 class AnalizadorTest: DescribeSpec( {
@@ -12,8 +14,9 @@ class AnalizadorTest: DescribeSpec( {
     val respuesta = Respuesta(CodigoHttp.OK,"hola mundo",12,pedido1)
     val respuesta2 = Respuesta(CodigoHttp.NOT_FOUND,"",10,pedidoConError)
     val respuesta3 = Respuesta(CodigoHttp.NOT_IMPLEMENTED,"",10,pedidoConError)
+    val respuesta4 = Respuesta(CodigoHttp.OK,"hola mundo",12,pedido1)
 
-    val modulo = Modulo(Tipo.HTML,12,"hola mundo")
+    val modulo1 = Modulo(Tipo.HTML,12,"hola mundo")
     val modulo2 = Modulo(Tipo.TEXTO,12,"hola mundo")
 
 
@@ -22,7 +25,11 @@ class AnalizadorTest: DescribeSpec( {
         describe("funciones basicas de los analizadores"){
             val analizador1 = AnalizadorDemora(8)
             it("se agregan modulos y respuestas de manera correcta"){
+                analizador1.agregarModuloYRespuesta(modulo1,respuesta)
+                analizador1.agregarModuloYRespuesta(modulo1,respuesta4)
+                analizador1.agregarModuloYRespuesta(modulo2,respuesta2)
 
+                analizador1.modulosYRespuestas.shouldNotBeNull()
             }
         }
 
